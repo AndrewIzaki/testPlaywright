cc32ba42ac035a63cd09a0f9085e38d3194ce580
Andrew <andrewtsuyoshizaki@gmail.com>
2026-04-26 19:05:33 -0300

    Primeiro teste visual criado

--- test/TaskList.spec.js
+++ test/TaskList.spec.js
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('criando item', async({page})=>{
-  await page.goto('http://127.0.0.1:3000/');
+//Item sendo criado na lista
+test('criando item', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8080/');
 
-  await page.locator('#btnAdd').click()
-})
+  const [msg] = await Promise.all([
+    page.waitForEvent('console'),
+    page.locator('#Item').fill('Tarefa 1'),
+    page.locator('#btnAdd').click()
+  ]);
+
+  expect(msg.text()).toBe('Item adicionado com sucesso');
+});
